15da0698f05d636f09fc0d7929a34d85ed2ae85f
Mansour Tarafdar <tarafdar.mansour@gmail.com>
2023-08-24 18:07:15 +0330

    fix(application): tax should calculate for every day

--- CongestionTaxCalculator.Application/GothenburgCongestionTaxCalculator.cs
+++ CongestionTaxCalculator.Application/GothenburgCongestionTaxCalculator.cs
@@ -11,38 +11,47 @@ public class GothenburgCongestionTaxCalculator
              * @param dates   - date and time of all passes on one day
              * @return - the total congestion tax for that day
              */
-    public int GetTax(Vehicle vehicle, DateTime[] dates)
+    public int GetTax(Vehicle vehicle, DateTime[] movementDateTimeInYear)
     {
-        var intervalStart = dates[0];
-        var totalFee = 0;
-        foreach (var date in dates)
+        if (IsTollFreeVehicle(vehicle)) return 0;
+
+        movementDateTimeInYear = movementDateTimeInYear.ToList().Order().ToArray();
+        var groupedDates = movementDateTimeInYear.GroupBy(d => d.Date);
+        int totalTax = 0;
+        foreach (var groupedDate in groupedDates)
         {
-            var nextFee = GetTollFee(date, vehicle);
-            var tempFee = GetTollFee(intervalStart, vehicle);
-
-            long diffInMillies = date.Millisecond - intervalStart.Millisecond;
-            var minutes = diffInMillies / 1000 / 60;
-
-            if (minutes <= 60)
+            var dayMovements = groupedDate.ToList();
+            DateTime intervalStart = dayMovements[0];
+            int taxOfDay = 0;
+            foreach (DateTime time in dayMovements)
             {
-                if (totalFee > 0) totalFee -= tempFee;
-                if (nextFee >= tempFee) tempFee = nextFee;
-                totalFee += tempFee;
+                int nextFee = GetTollFee(time);
+                int tempFee = GetTollFee(intervalStart);
+
+                long diffInMillies = time.Millisecond - intervalStart.Millisecond;
+                long minutes = diffInMillies / 1000 / 60;
+
+                if (minutes <= 60)
+                {
+                    if (taxOfDay > 0) taxOfDay -= tempFee;
+                    if (nextFee >= tempFee) tempFee = nextFee;
+                    taxOfDay += tempFee;
+                }
+                else
+                {
+                    taxOfDay += nextFee;
+                }
             }
-            else
-            {
-                totalFee += nextFee;
-            }
+            if (taxOfDay > 60) taxOfDay = 60;
+            totalTax += taxOfDay;
         }
-
-        if (totalFee > 60) totalFee = 60;
-        return totalFee;
+        return totalTax;
     }
 
     private bool IsTollFreeVehicle(Vehicle vehicle)
     {
         if (vehicle == null) return false;
-        var vehicleType = vehicle.GetVehicleType();
+        String vehicleType = vehicle.GetVehicleType();
         return vehicleType.Equals(TollFreeVehicles.Motorcycle.ToString()) ||
                vehicleType.Equals(TollFreeVehicles.Tractor.ToString()) ||
                vehicleType.Equals(TollFreeVehicles.Emergency.ToString()) ||
@@ -51,43 +60,47 @@ public class GothenburgCongestionTaxCalculator
                vehicleType.Equals(TollFreeVehicles.Military.ToString());
     }
 
-    public int GetTollFee(DateTime date, Vehicle vehicle)
+    public int GetTollFee(DateTime date)
     {
-        if (IsTollFreeDate(date) || IsTollFreeVehicle(vehicle)) return 0;
+        if (IsTollFreeDate(date)) return 0;
 
-        var hour = date.Hour;
-        var minute = date.Minute;
+        int hour = date.Hour;
+        int minute = date.Minute;
 
         if (hour == 6 && minute >= 0 && minute <= 29) return 8;
-        if (hour == 6 && minute >= 30 && minute <= 59) return 13;
-        if (hour == 7 && minute >= 0 && minute <= 59) return 18;
-        if (hour == 8 && minute >= 0 && minute <= 29) return 13;
-        if (hour >= 8 && hour <= 14 && minute >= 30 && minute <= 59) return 8;
-        if (hour == 15 && minute >= 0 && minute <= 29) return 13;
-        if ((hour == 15 && minute >= 0) || (hour == 16 && minute <= 59)) return 18;
-        if (hour == 17 && minute >= 0 && minute <= 59) return 13;
-        if (hour == 18 && minute >= 0 && minute <= 29) return 8;
-        return 0;
+        else if (hour == 6 && minute >= 30 && minute <= 59) return 13;
+        else if (hour == 7 && minute >= 0 && minute <= 59) return 18;
+        else if (hour == 8 && minute >= 0 && minute <= 29) return 13;
+        else if (hour >= 8 && hour <= 14 && minute >= 30 && minute <= 59) return 8;
+        else if (hour == 15 && minute >= 0 && minute <= 29) return 13;
+        else if (hour == 15 && minute >= 0 || hour == 16 && minute <= 59) return 18;
+        else if (hour == 17 && minute >= 0 && minute <= 59) return 13;
+        else if (hour == 18 && minute >= 0 && minute <= 29) return 8;
+        else return 0;
     }
 
-    private bool IsTollFreeDate(DateTime date)
+    private Boolean IsTollFreeDate(DateTime date)
     {
-        var year = date.Year;
-        var month = date.Month;
-        var day = date.Day;
+        int year = date.Year;
+        int month = date.Month;
+        int day = date.Day;
 
         if (date.DayOfWeek == DayOfWeek.Saturday || date.DayOfWeek == DayOfWeek.Sunday) return true;
 
         if (year == 2013)
-            if ((month == 1 && day == 1) ||
-                (month == 3 && (day == 28 || day == 29)) ||
-                (month == 4 && (day == 1 || day == 30)) ||
-                (month == 5 && (day == 1 || day == 8 || day == 9)) ||
-                (month == 6 && (day == 5 || day == 6 || day == 21)) ||
+        {
+            if (month == 1 && day == 1 ||
+                month == 3 && (day == 28 || day == 29) ||
+                month == 4 && (day == 1 || day == 30) ||
+                month == 5 && (day == 1 || day == 8 || day == 9) ||
+                month == 6 && (day == 5 || day == 6 || day == 21) ||
                 month == 7 ||
-                (month == 11 && day == 1) ||
-                (month == 12 && (day == 24 || day == 25 || day == 26 || day == 31)))
+                month == 11 && day == 1 ||
+                month == 12 && (day == 24 || day == 25 || day == 26 || day == 31))
+            {
                 return true;
+            }
+        }
         return false;
     }
 
